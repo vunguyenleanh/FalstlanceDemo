b2be91f6ceee2f4102aa74c043691518d1143645
nlavuit <nlavuit@gmail.com>
2018-03-09 14:49:19 +0700

    upload demo

---
+++ ios/Pods/Target Support Files/Pods-FastlanceDemo-tvOS/Pods-FastlanceDemo-tvOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FastlanceDemo_tvOS : NSObject
+@end
+@implementation PodsDummy_Pods_FastlanceDemo_tvOS
+@end
---
+++ ios/Pods/Target Support Files/Pods-FastlanceDemoTests/Pods-FastlanceDemoTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FastlanceDemoTests : NSObject
+@end
+@implementation PodsDummy_Pods_FastlanceDemoTests
+@end
